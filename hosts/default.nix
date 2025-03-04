31652f3855b9e9d24102719250f116bf10d52623
Henri Rosten <henri.rosten@unikie.com>
2025-03-04 15:08:16 +0200

    Initial commit
    Signed-off-by: Henri Rosten <henri.rosten@unikie.com>

---
+++ hosts/default.nix
@@ -0,0 +1,63 @@
+{
+  inputs,
+  self,
+  ...
+}:
+let
+  specialArgs = {
+    inherit inputs self;
+  };
+in
+{
+  flake.nixosModules = {
+    hosts-common = import ./hosts-common.nix;
+    nixos-builder = ./builder/conf.nix;
+    nixos-jenkins-controller = ./jenkins-controller/conf.nix;
+  };
+  flake.nixosConfigurations = {
+
+    vm-builder = inputs.nixpkgs.lib.nixosSystem {
+      inherit specialArgs;
+      modules = [
+        (import ./vm-nixos-qemu.nix { })
+        self.nixosModules.nixos-builder
+        {
+          # https://github.com/NixOS/nixpkgs/blob/master/nixos/modules/virtualisation/qemu-vm.nix
+          virtualisation.vmVariant.virtualisation.forwardPorts = [
+            {
+              from = "host";
+              host.port = 2322;
+              guest.port = 22;
+            }
+          ];
+        }
+      ];
+    };
+
+    vm-jenkins-controller = inputs.nixpkgs.lib.nixosSystem {
+      inherit specialArgs;
+      modules = [
+        (import ./vm-nixos-qemu.nix {
+          ram_gb = 20;
+          disk_gb = 150;
+        })
+        self.nixosModules.nixos-jenkins-controller
+        {
+          # https://github.com/NixOS/nixpkgs/blob/master/nixos/modules/virtualisation/qemu-vm.nix
+          virtualisation.vmVariant.virtualisation.forwardPorts = [
+            {
+              from = "host";
+              host.port = 8081;
+              guest.port = 8081;
+            }
+            {
+              from = "host";
+              host.port = 2222;
+              guest.port = 22;
+            }
+          ];
+        }
+      ];
+    };
+  };
+}
---
+++ nix/apps.nix
@@ -0,0 +1,50 @@
+{
+  inputs,
+  self,
+  ...
+}:
+let
+  pkgs = import inputs.nixpkgs { system = "x86_64-linux"; };
+  decrypt-sops-key = pkgs.writeShellScript "decrypt-sops-key" ''
+    set -eu
+    on_err () {
+      echo "[+] Failed decrypting sops key: VM will boot-up without secrets"
+      # Wait for user input if stdout is to a terminal (and not to file or pipe)
+      if [ -t 1 ]; then
+        echo; read -n 1 -srp "Press any key to continue"; echo
+      fi
+      exit 1
+    }
+    trap on_err ERR
+    if [ $# -ne 2 ] || [ -z "$1" ] || [ -z "$2" ]; then
+      on_err
+    fi
+    secret="$1"
+    todir="$2"
+    mkdir -p "$todir"
+    rm -fr "$todir/ssh_host_ed25519_key"
+    tofile="$todir/ssh_host_ed25519_key"
+    umask 377
+    ${pkgs.lib.getExe pkgs.sops} --extract '["ssh_host_ed25519_key"]' --decrypt "$secret" >"$tofile"
+    echo "[+] Decrypted sops key '$tofile'"
+  '';
+in
+{
+  flake.apps."x86_64-linux" = {
+    run-vm-builder = {
+      type = "app";
+      program = self.nixosConfigurations.vm-builder.config.system.build.vm;
+    };
+    run-vm-jenkins-controller = {
+      type = "app";
+      program = pkgs.writeShellScriptBin "jenkins-nixosvm-with-secrets" ''
+        echo "[+] Running $(realpath "$0")"
+        secret="${self.outPath}/hosts/jenkins-controller/secrets.yaml"
+        todir="${self.nixosConfigurations.vm-jenkins-controller.config.virtualisation.vmVariant.virtualisation.sharedDirectories.shr.source}"
+        ${decrypt-sops-key} "$secret" "$todir"
+        ${pkgs.lib.getExe self.nixosConfigurations.vm-jenkins-controller.config.system.build.vm}
+        rm -fr "$todir/ssh_host_ed25519_key"
+      '';
+    };
+  };
+}
